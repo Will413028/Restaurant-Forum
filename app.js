c96b57924f4ce04ff4abde94e7c715213172fc31
william1993314 <william1993314@gmail.com>
2021-09-14 17:55:35 -0400

    Initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars())
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
@@ -13,4 +14,3 @@ app.listen(port, () => {
 require('./routes')(app)
 
 module.exports = app
-
