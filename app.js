f1879114edd9eb8e040de5a65f3fb2b33e00c863
william1993314 <william1993314@gmail.com>
2021-09-15 04:12:26 -0400

    Add admin edit function

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-
 const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
@@ -17,6 +17,7 @@ app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
+app.use(methodOverride('_method'))
 
 app.use((req, res, next) => {
     res.locals.success_messages = req.flash('success_messages')
