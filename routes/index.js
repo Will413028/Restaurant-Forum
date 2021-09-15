45e93d1abb0e29618315d2e978299fddffbc13ac
william1993314 <william1993314@gmail.com>
2021-09-15 17:27:48 -0400

    Add admin categories page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const helpers = require('../_helpers')
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
+const categoryController = require('../controllers/categoryController.js')
 
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
@@ -35,6 +36,9 @@ module.exports = (app, passport) => {
     app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
     app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
     app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
+    app.get('/admin/users', authenticatedAdmin, adminController.getUsers)
+    app.put('/admin/users/:id', authenticatedAdmin, adminController.putUsers)
+    app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
 
     app.get('/signup', userController.signUpPage)
     app.post('/signup', userController.signUp)
