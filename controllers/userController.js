d3ba86482a85c84e390e7a588b45ec3af7814032
william1993314 <william1993314@gmail.com>
2021-09-17 03:00:32 -0400

    Add favorite / Delete favorite function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,28 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
+const Restaurant = db.Restaurant
+const Comment = db.Comment
+const Favorite = db.Favorite
+
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const userController = {
     signUpPage: (req, res) => {
         return res.render('signup')
     },
+
     signUp: (req, res) => {
         // confirm password
-        if(req.body.passwordCheck !== req.body.password){
+        if (req.body.passwordCheck !== req.body.password) {
             req.flash('error_messages', '兩次密碼輸入不同！')
             return res.redirect('/signup')
         } else {
             // confirm unique user
-            User.findOne({where: {email: req.body.email}}).then(user => {
-                if(user){
-                    req.flash('error_messages', 'Email has already register!！')
+            User.findOne({ where: { email: req.body.email } }).then(user => {
+                if (user) {
+                    req.flash('error_messages', '信箱重複！')
                     return res.redirect('/signup')
                 } else {
                     User.create({
@@ -23,7 +30,7 @@ const userController = {
                         email: req.body.email,
                         password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
                     }).then(user => {
-                        req.flash('success_messages', 'Registration successful！')
+                        req.flash('success_messages', '成功註冊帳號！')
                         return res.redirect('/signin')
                     })
                 }
@@ -35,14 +42,80 @@ const userController = {
     },
 
     signIn: (req, res) => {
-        req.flash('success_messages', 'Login successful！')
+        req.flash('success_messages', '成功登入！')
         res.redirect('/restaurants')
     },
 
     logout: (req, res) => {
-        req.flash('success_messages', 'Logout successful！')
+        req.flash('success_messages', '登出成功！')
         req.logout()
         res.redirect('/signin')
+    },
+    getUser: (req, res) => {
+        return User.findByPk(req.params.id, {
+            include: [
+                { model: Comment, include: Restaurant }
+            ]
+        }).then(user => {
+            return res.render('users/profile', { profile: user.toJSON() })
+        })
+    },
+    editUser: (req, res) => {
+        return User.findByPk(req.params.id).then(user => {
+            return res.render('users/edit', { user: user.toJSON() })
+        })
+    },
+    putUser: (req, res) => {
+        if (Number(req.params.id) !== Number(req.user.id)) {
+            return res.redirect(`/users/${req.params.id}`)
+        }
+        const { file } = req
+        if (file) {
+            imgur.setClientID(IMGUR_CLIENT_ID)
+            imgur.upload(file.path, (err, img) => {
+                return User.findByPk(req.params.id)
+                    .then((user) => {
+                        user.update({
+                            name: req.body.name,
+                            image: img.data.link
+                        }).then((user) => {
+                            res.redirect(`/users/${req.params.id}`)
+                        })
+                    })
+            })
+        } else {
+            return User.findByPk(req.params.id)
+                .then((user) => {
+                    user.update({
+                        name: req.body.name
+                    }).then((user) => {
+                        res.redirect(`/users/${req.params.id}`)
+                    })
+                })
+        }
+    },
+    addFavorite: (req, res) => {
+        return Favorite.create({
+            UserId: req.user.id,
+            RestaurantId: req.params.restaurantId
+        })
+            .then((restaurant) => {
+                return res.redirect('back')
+            })
+    },
+    removeFavorite: (req, res) => {
+        return Favorite.findOne({
+            where: {
+                UserId: req.user.id,
+                RestaurantId: req.params.restaurantId
+            }
+        })
+            .then((favorite) => {
+                favorite.destroy()
+                    .then((restaurant) => {
+                        return res.redirect('back')
+                    })
+            })
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -30,8 +30,17 @@ module.exports = (app, passport) => {
     app.get('/restaurants', authenticated, restController.getRestaurants)
     app.get('/restaurants/feeds', authenticated, restController.getFeeds)
     app.get('/restaurants/:id', authenticated, restController.getRestaurant)
+    app.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
+
     app.post('/comments', authenticated, commentController.postComment)
     app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
+
+    app.get('/users/:id', authenticated, userController.getUser)
+    app.get('/users/:id/edit', authenticated, userController.editUser)
+    app.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+
+    app.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+    app.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
 
     app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
     app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
